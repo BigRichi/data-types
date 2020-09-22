ef2efd54101d334b98db93c1f2e503052d065dc4
BigRichi <big.richi@outlook.com>
2020-09-22 20:13:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#A dtat type is a value
+#fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes => custom Data TYpes
+
+#Specialized Data Types are not built into python however we can use them from Modules
+
+#None = 'nothing'
